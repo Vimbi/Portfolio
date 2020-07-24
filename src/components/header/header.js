8d0805e76a1806a91f17bf80ebc83c5611f899da
Vimbi <i.am.vimbi@gmail.com>
2020-07-24 15:43:52 +0300

    logo add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -7,8 +7,11 @@ export default class Header extends Component {
   render () {
     return (
       <div className='header'>
+        <div className='logo'>
+          <img src={require('../img/logo.png')} alt='logo'></img>
+        </div>
         <h3>Header</h3>
-        <nav>
+        <nav className='nav-wrapper'>
           <ul>
             <li>Menu 1</li>
             <li>Menu 2</li>
